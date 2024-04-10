41e04d98a61f57cd8d322ed4f3011b13edb42744
emotytto00 <johannes.liikanen@gmail.com>
2024-04-10 11:04:37 +0300

    maybe done

---
+++ assignment1/app.js
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import express from 'express';
+import process from 'process';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: 'Fluffy',
+    birthdate: '2022-01-01',
+    weight: 5,
+    owner: 'John Doe',
+    image: 'https://loremflickr.com/320/240/cat',
+  };
+  res.json(cat);
+});
+
+app.use('/public', express.static('public'));
